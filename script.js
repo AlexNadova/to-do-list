5dcbfbd546a476dda40f53ada016c47d6c79daf8
Alexandra Nadova <alexandranadova@gmail.com>
2020-09-24 10:36:03 +0200

    add js validation

--- script.js
+++ script.js
@@ -4,14 +4,20 @@ var list = document.getElementById("toDoList");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   var input = form.elements[0];
-  let newListItem = document.createElement("li");
-  //for generating ids
-  let currentDate = new Date();
-  newListItem.setAttribute("id", currentDate.getTime());
-  //add text from input
-  newListItem.innerText = input.value;
-  //clear input
-  input.value = "";
-  //add item to the list
-  list.appendChild(newListItem);
+  //check if input contains a value - in case html required is deleted
+  if (input.validity.valueMissing) {
+    input.setCustomValidity("");
+    let newListItem = document.createElement("li");
+    //for generating ids
+    let currentDate = new Date();
+    newListItem.setAttribute("id", currentDate.getTime());
+    //add text from input
+    newListItem.innerText = input.value;
+    //clear input
+    input.value = "";
+    //add item to the list
+    list.appendChild(newListItem);
+  } else {
+    input.setCustomValidity("Please, input a value.");
+  }
 });
